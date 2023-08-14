e25acbbad2630e1b6b281c4ab6cdcb80908118d2
AlanHenriqueRF <alhenrangelfera@gmail.com>
2023-08-14 17:48:47 -0300

    Fix: to deploy

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { styled } from "styled-components"
 import Apiperfil from "../service/perfil";
